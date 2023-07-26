ea9a09c1c7da1ebf532526933ee7980f6b193d86
mdmujibalam <mujibalam2000@gmail.com>
2023-07-26 13:18:30 +0530

    Lecture 5 Completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ const Footer = ()=>{
     return (
       <div className="footer">
       Created By
-      <i class="fa-solid fa-heart"></i>
+      <i className="fa-solid fa-heart"></i>
       <a href="https://www.linkedin.com/in/mdmujibalam/" target="_blank">
         Md Mujib Alam
       </a>
-      <i class="fa-solid fa-copyright"></i>2023
+      <i className="fa-solid fa-copyright"></i>2023
       <strong>
         Food<span>Fire</span>
       </strong>
